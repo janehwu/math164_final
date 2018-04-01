359b2181b2c032b5b282029e647d01988a1d4e98
Jane Wu <jhwu@hmc.edu>
2018-03-31 21:31:29 -0700

    Actually encrypting now

--- encrypt.py
+++ encrypt.py
@@ -1,11 +1,17 @@
+import random
+
 import loadData
 
 def encrypt(filename):
-	encrypted_string = ""
+	random.seed(1234) # So its the same every time
 
+	in_string = ""
+
+	# Re-format original text
 	with open(filename) as fn:
 		prev_i = -1
 		prev_c = None
+
 		for line in fn:
 			words = line.split(",")
 			for w in words:
@@ -17,14 +23,33 @@ def encrypt(filename):
 					if (prev_i == 26) and (cur_i == 26): # if multiple spaces
 						continue
 					if prev_c == None:
-						encrypted_string += c.lower()
+						in_string += c.lower()
 
 					if prev_i != -1: # if valid previous char
 						#print(prev_c, prev_i, c, cur_i)
-						encrypted_string += c.lower()
+						in_string += c.lower()
 					prev_c = c
 					prev_i = cur_i
+
+	# Perform encryption
+	mapping = [i for i in range(27)]
+	random.shuffle(mapping)
+	encrypted_string = ''
+
+	# Encrypt each character
+	for i in range(len(in_string)):
+		encrypted_string += encryptChar(in_string[i], mapping)
 	return encrypted_string
+
+def encryptChar(c, mapping):
+	i = loadData.getIndex(c)
+	if i != -1:
+		new_i = mapping[i]
+		if new_i == 26:
+			return ' '
+		else:
+			return chr(new_i + 97)
+
 
 # To test
 #print(encrypt('prideprejudice.txt'))
