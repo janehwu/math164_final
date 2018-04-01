3f0a24a704fdfa3cce33d43b06d80d305362b836
Jane Wu <jhwu@hmc.edu>
2018-03-31 21:09:02 -0700

    Fixed bugs

--- loadData.py
+++ loadData.py
@@ -5,31 +5,37 @@ def getMatrix(filename):
 	M = np.zeros((27,27))
 
 	with open(filename) as fn:
+		prev_i = -1
+		prev_c = None
 		for line in fn:
-			words = line.split(" ")
-			if words[0] == 'CHAPTER': # Ignore if chapter title
+			words = line.split(",")
+			if words[0][:7] == 'CHAPTER': # Ignore if chapter title
 				continue
 
-			prev_i = getIndex(words[0][0]) # First character of text
-			words[0] = words[0][1:] # Ignore first character now
-		
 			for w in words:
 				for c in w:
 					cur_i = getIndex(c)
-					if prev_i == 26 and cur_i == 26: # previous character was also non-alphabetical
+
+					if cur_i == -1: # if character not letter or space
 						continue
-					else:
-						#print(prev_i, cur_i)
+					if (prev_i == 26) and (cur_i == 26): # if multiple spaces
+						continue
+
+					if prev_i != -1: # if valid previous char
+						#print(prev_c, prev_i, c, cur_i)
 						M[prev_i][cur_i] += 1
+					prev_c = c
 					prev_i = cur_i
-	#return M
+	return M
 
 
 def getIndex(c):
 	ascii = ord(c.lower())
 	if ascii >= 97 and ascii <= 122:
 		return ascii - 97 # a is index 0
-	else:
-		return 26 # other character
+	elif ascii == 32 or ascii == 10: # space or newline
+		return 26 # space
+	return -1
 
-print(getMatrix('test.txt'))
+# To test
+#getMatrix('warpeace.txt')
